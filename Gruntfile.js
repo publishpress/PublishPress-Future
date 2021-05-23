c61737819c47de34b6c8878260d2e2da98523974
Ashish Ravi <contactashish13@gmail.com>
2021-05-23 18:50:01 +0530

    fix #19: wpcs compliant code

---
+++ Gruntfile.js
@@ -0,0 +1,115 @@
+module.exports = function(grunt) {
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        files_php: [
+            '*.php',
+            '**/*.php',
+            '!.git/**',
+            '!vendor/**',
+            '!node_modules/**',
+            '!logs/**',
+            '!.git/**',
+        ],
+        files_js: [
+            '*.js',
+            '**/*.js',
+            '!.git/**',
+            '!vendor/**',
+            '!node_modules/**',
+            '!logs/**',
+            '!Gruntfile.js',
+            '!.git/**',
+        ],
+        mkdir: {
+            logs: {
+                options: {
+                    create: ['logs']
+                }
+            }
+        },
+        phpcs: {
+            options: {
+                bin: 'vendor/bin/phpcs',
+                standard: 'phpcs.xml',
+                reportFile: 'logs/phpcs.log',
+                extensions: 'php',
+                showSniffCodes: true
+            },
+            src: [
+                '<%= files_php %>'
+            ]
+        },
+        phpcbf: {
+            options: {
+                bin: 'vendor/bin/phpcbf',
+                standard: 'phpcs.xml',
+                noPatch: false,
+                extensions: 'php'
+            },
+            src: [
+                '<%= files_php %>'
+            ]
+        },
+        phplint: {
+            options: {
+                standard: 'phpcs.xml'
+            },
+            src: [
+                '<%= files_php %>'
+            ]
+        },
+        jshint: {
+            options: {
+                jshintrc: true,
+                reporterOutput: 'logs/jslogs.log'
+            },
+            all: [
+                '<%= files_js %>'
+            ]
+        },
+        pot_config: '<%= pkg.pot %>',
+        makepot: {
+            target: {
+                options: {
+                    mainFile: 'post-expirator.php',
+                    type: 'wp-plugin',
+                    updateTimestamp: false,
+                    processPot: function( pot, options ) {
+                        // https://github.com/cedaro/grunt-wp-i18n/blob/develop/docs/examples/remove-package-metadata.md
+                        var translation,
+                        excluded_meta = [
+                            'Plugin Name of the plugin/theme',
+                            'Plugin URI of the plugin/theme',
+                            'Author of the plugin/theme',
+                            'Author URI of the plugin/theme'
+                        ];
+
+                        for ( translation in pot.translations[''] ) {
+                            if ( 'undefined' !== typeof pot.translations[''][ translation ].comments.extracted ) {
+                                if ( excluded_meta.indexOf( pot.translations[''][ translation ].comments.extracted ) >= 0 ) {
+                                    console.log( 'Excluded meta: ' + pot.translations[''][ translation ].comments.extracted );
+                                    delete pot.translations[''][ translation ];
+                                }
+                            }
+                        }
+
+                        // additional
+                        pot.headers['last-translator'] = grunt.config.get('pot_config').lasttranslator;
+                        pot.headers['language-team'] = grunt.config.get('pot_config').languageteam;
+                        delete pot.headers['lang-translator'];
+                        delete pot.headers['x-generator'];
+                        delete pot.headers['po-revision-date'];
+                        return pot;
+                    }
+                }
+            }
+        }
+    });
+
+    // default tasks
+    grunt.registerTask('default', ['mkdir', 'phpcbf', 'phpcs', 'phplint', 'jshint', 'makepot']);
+
+};
--- post-expirator-debug.php
+++ post-expirator-debug.php
@@ -1,64 +1,88 @@
 <?php
 
-class postExpiratorDebug {
+// phpcs:disable WordPress.DB.PreparedSQL.NotPrepared, WordPress.DB.PreparedSQL.InterpolatedNotPrepared
 
+/**
+ * The class that adds debug entries to the database.
+ */
+class PostExpiratorDebug {
+
+	/**
+	 * Constructor.
+	 */
 	function __construct() {
 		global $wpdb;
 		$this->debug_table = $wpdb->prefix . 'postexpirator_debug';
 		$this->createDBTable();
 	}
-	
+
 	/**
 	 * Create Database Table to store debugging information if it does not already exist.
 	 */
 	private function createDBTable() {
 		global $wpdb;
 
-		if ($wpdb->get_var("SHOW TABLES LIKE '".$this->debug_table."'") != $this->debug_table) {
-			$sql = "CREATE TABLE `".$this->debug_table."` (
+		if ( $wpdb->get_var( "SHOW TABLES LIKE '" . $this->debug_table . "'" ) !== $this->debug_table ) {
+			$sql = 'CREATE TABLE `' . $this->debug_table . '` (
 				`id` INT(9) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 				`timestamp` TIMESTAMP NOT NULL,
 				`blog` INT(9) NOT NULL,
 				`message` text NOT NULL
-			);";
-			require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-			dbDelta($sql);
+			);';
+			require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+			dbDelta( $sql );
 		}
 	}
 
+	/**
+	 * Drop Database Table.
+	 */
 	public function removeDBTable() {
 		global $wpdb;
-		$wpdb->query('DROP TABLE IF EXISTS '.$this->debug_table);
-	}	
-
-	public function save($data) {
-		global $wpdb;
-		if (is_multisite()) {
-			global $current_blog;
-			$blog = $current_blog->blog_id;
-		} else $blog = 0;
-		$wpdb->query($wpdb->prepare('INSERT INTO '.$this->debug_table.' (`timestamp`,`message`,`blog`) VALUES (FROM_UNIXTIME(%d),%s,%s)',time(),$data['message'],$blog));
+		$wpdb->query( 'DROP TABLE IF EXISTS ' . $this->debug_table );
 	}
 
+	/**
+	 * Insert into Database Table.
+	 */
+	public function save( $data ) {
+		global $wpdb;
+		if ( is_multisite() ) {
+			global $current_blog;
+			$blog = $current_blog->blog_id;
+		} else {
+			$blog = 0;
+		}
+		$wpdb->query( $wpdb->prepare( 'INSERT INTO ' . $this->debug_table . ' (`timestamp`,`message`,`blog`) VALUES (FROM_UNIXTIME(%d),%s,%s)', time(), $data['message'], $blog ) );
+	}
+
+	/**
+	 * Get the HTML of the table's data.
+	 */
 	public function getTable() {
 		global $wpdb;
-		$results = $wpdb->get_results("SELECT * FROM {$this->debug_table} ORDER BY `id` DESC");
-		if (empty($results)) {
-			print '<p>'.__('Debugging table is currently empty.','post-expirator').'</p>';
+		$results = $wpdb->get_results( "SELECT * FROM {$this->debug_table} ORDER BY `id` DESC" );
+		if ( empty( $results ) ) {
+			print '<p>' . __( 'Debugging table is currently empty.', 'post-expirator' ) . '</p>';
 			return;
 		}
 		print '<table class="post-expirator-debug">';
-		print '<tr><th class="post-expirator-timestamp">'.__('Timestamp','post-expirator').'</th>';
-		print '<th>'.__('Message','post-expirator').'</th></tr>';
-		foreach ($results as $result) {
-			print '<tr><td>'.$result->timestamp.'</td>';
-			print '<td>'.$result->message.'</td></tr>';			
+		print '<tr><th class="post-expirator-timestamp">' . __( 'Timestamp', 'post-expirator' ) . '</th>';
+		print '<th>' . __( 'Message', 'post-expirator' ) . '</th></tr>';
+		foreach ( $results as $result ) {
+			print '<tr><td>' . $result->timestamp . '</td>';
+			print '<td>' . $result->message . '</td></tr>';
 		}
 		print '</table>';
 	}
 
+	/**
+	 * Truncate Database Table.
+	 */
 	public function purge() {
 		global $wpdb;
-		$wpdb->query("TRUNCATE TABLE {$this->debug_table}");		
+		$wpdb->query( "TRUNCATE TABLE {$this->debug_table}" );
 	}
 }
+
+// phpcs:enable
