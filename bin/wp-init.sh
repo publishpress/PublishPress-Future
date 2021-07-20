42a50ec713122a4a6c553ab2ad1d18d9f8898a50
Ashish Ravi <contactashish13@gmail.com>
2021-07-20 14:15:02 +0530

    take inspiration from https://github.com/google/site-kit-wp/

--- bin/wp-init.sh
+++ bin/wp-init.sh
@@ -1,57 +1,101 @@
 #!/usr/bin/env bash
 
+##
+# Docker Compose helper
+#
+# Calls docker-compose with common options.
+##
+dc() {
+    docker-compose -f ../docker-compose.yml "$@"
+}
+
+##
+# WP CLI
+#
+# Executes a WP CLI request in the CLI container.
+##
+wp() {
+    dc exec $args cli wp --allow-root "$@"
+}
+
+##
+# WordPress Container helper.
+#
+# Executes the given command in the wordpress container.
+##
+container() {
+    dc exec $args pe_wordpress "$@"
+}
+
 wp_host='localhost'
+wp_port='80'
 
 if [[ -z "${PE_WP_HOST}" ]]; then
     # if on windows, find out the IP using `docker-machine ip` and provide the IP as the host.
     windows=`echo $OSTYPE | grep -i -e "win" -e "msys" -e "cygw" | wc -l`
-    args='';
     if [[ $windows -gt 0 ]]; then
         wp_host=`docker-machine ip`
-        args=''
     fi
 else
     wp_host="${PE_WP_HOST}"
 fi
 
-npx wait-on --timeout 30000 "http://$wp_host"
+# Get the host port for the WordPress container.
+wp_port=$(dc port $CONTAINER 80 | awk -F : '{printf $2}')
 
-# permissions
-docker exec $args pe_wordpress chown -R www-data:www-data /var/www/html/
-docker exec $args pe_wordpress chmod 0777 -R /var/www/html/wp-content
+npx wait-on --timeout 30000 "http://$wp_host:$wp_port"
+
+
+if [ -t 0 ]; then
+    args=""
+else
+    args="-T" # Disable pseudo-tty allocation. By default `docker-compose exec` allocates a TTY.
+fi
 
 # install WP
-docker exec $args pe_wordpress wp --allow-root core install --url="http://$wp_host/" --admin_user="wordpress" --admin_password="wordpress" --admin_email="test1@xx.com" --title="test" --skip-email
+wp core install --url="http://$wp_host:$wp_port/" --admin_user="wordpress" --admin_password="wordpress" --admin_email="test1@xx.com" --title="test" --skip-email
+
+container mkdir -p \
+	/var/www/html/wp-content/uploads \
+	/var/www/html/wp-content/upgrade
+container chmod 767 \
+	/var/www/html/wp-content \
+	/var/www/html/wp-content/plugins \
+	/var/www/html/wp-config.php \
+	/var/www/html/wp-settings.php \
+	/var/www/html/wp-content/uploads \
+	/var/www/html/wp-content/upgrade
 
 # update core
-docker exec $args pe_wordpress wp --allow-root option delete core_updater.lock
+wp option delete core_updater.lock
 if [[ -z "${PE_WP_VERSION}" ]]; then
-    docker exec $args pe_wordpress wp --allow-root core update
+    wp core update
 else
     wp_version="${PE_WP_VERSION}"
-    docker exec $args pe_wordpress wp --allow-root core update --version=$wp_version --force
+    wp core update --version=$wp_version --force
 fi
-docker exec $args pe_wordpress wp --allow-root core update-db
+wp core update-db
 
 # activate
-docker exec $args pe_wordpress wp --allow-root plugin activate post-expirator
+wp theme activate twentytwenty || wp theme install --activate twentytwenty
+wp plugin activate post-expirator
 
 # debugging
-docker exec $args pe_wordpress wp --allow-root config set WP_DEBUG true --raw
-docker exec $args pe_wordpress wp --allow-root config set WP_DEBUG_LOG true --raw
-docker exec $args pe_wordpress wp --allow-root config set WP_DEBUG_DISPLAY false --raw
+wp config set WP_DEBUG true --raw
+wp config set WP_DEBUG_LOG true --raw
+wp config set WP_DEBUG_DISPLAY false --raw
 
 # add some categories
-docker exec $args pe_wordpress wp --allow-root term create category apple
-docker exec $args pe_wordpress wp --allow-root term create category banana
-docker exec $args pe_wordpress wp --allow-root term create category custard
+wp term create category apple
+wp term create category banana
+wp term create category custard
 
 # add a post with ID: 1000 and add to apple
-docker exec $args pe_wordpress wp --allow-root post create --post_type=post --post_title="post1" --post_content="random" --post_status=publish --post_category="apple" --import_id=1000
-docker exec $args pe_wordpress wp --allow-root post term remove 1000 category 'uncategorized'
+wp post create --post_type=post --post_title="post1" --post_content="random" --post_status=publish --post_category="apple" --import_id=1000
+wp post term remove 1000 category 'uncategorized'
 
 # add a post with ID: 1001 and add to banana
-docker exec $args pe_wordpress wp --allow-root post create --post_type=post --post_title="post2" --post_content="random" --post_status=publish --post_category="banana" --import_id=1001
-docker exec $args pe_wordpress wp --allow-root post term remove 1000 category 'uncategorized'
+wp post create --post_type=post --post_title="post2" --post_content="random" --post_status=publish --post_category="banana" --import_id=1001
+wp post term remove 1000 category 'uncategorized'
 
-#docker exec $args pe_wordpress bash -c "cd /var/www/html/wp-content/plugins/post-expirator; ls -ltr; export show=false url=http://wp.lvh.me:80/; npm install; npm run e2e:run"
+
