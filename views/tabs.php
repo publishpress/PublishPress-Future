e033b6b34dc5a57b2f1e36bef0fde6c3bfd02bdd
Ashish Ravi <contactashish13@gmail.com>
2021-09-08 13:51:58 +0530

    fix #99: Rename "General Settings" to "Defaults"

--- views/tabs.php
+++ views/tabs.php
@@ -6,7 +6,7 @@
 	<h2><?php __( 'Post Expirator Options', 'post-expirator' ); ?></h2>
 	<div id="pe-settings-tabs">
 		<nav class="nav-tab-wrapper postexpirator-nav-tab-wrapper">
-			<a href="<?php echo admin_url( 'options-general.php?page=post-expirator.php&tab=general' ); ?>" class="pe-tab nav-tab <?php echo ( $current_tab === 'general' ? 'nav-tab-active' : '' ); ?>"><?php _e( 'General Settings', 'post-expirator' ); ?></a>
+			<a href="<?php echo admin_url( 'options-general.php?page=post-expirator.php&tab=general' ); ?>" class="pe-tab nav-tab <?php echo ( $current_tab === 'general' ? 'nav-tab-active' : '' ); ?>"><?php _e( 'Defaults', 'post-expirator' ); ?></a>
 			<a href="<?php echo admin_url( 'options-general.php?page=post-expirator.php&tab=defaults' ); ?>" class="pe-tab nav-tab <?php echo ( $current_tab === 'defaults' ? 'nav-tab-active' : '' ); ?>"><?php _e( 'Post Types', 'post-expirator' ); ?></a>
 			<a href="<?php echo admin_url( 'options-general.php?page=post-expirator.php&tab=display' ); ?>" class="pe-tab nav-tab <?php echo ( $current_tab === 'display' ? 'nav-tab-active' : '' ); ?>"><?php _e( 'Display', 'post-expirator' ); ?></a>
 			<a href="<?php echo admin_url( 'options-general.php?page=post-expirator.php&tab=diagnostics' ); ?>" class="pe-tab nav-tab <?php echo ( $current_tab === 'diagnostics' ? 'nav-tab-active' : '' ); ?>"><?php _e( 'Diagnostics', 'post-expirator' ); ?></a>
