403af50b9126cd41cb508be4dc4b4c453f4486b7
andergmartins <andergmartins@gmail.com>
2021-12-02 10:41:54 -0300

    Fix code style in a view

--- views/expire-column.php
+++ views/expire-column.php
@@ -54,30 +54,13 @@ echo esc_attr($id); ?>"
     // the hidden fields will be used by quick edit
 
     ?>
-    <?php
-    echo esc_html($display); ?>
-    <span id="expirationdate_year-<?php
-    echo $id; ?>" style="display: none;"><?php
-        echo $year; ?></span>
-    <span id="expirationdate_month-<?php
-    echo $id; ?>" style="display: none;"><?php
-        echo $month; ?></span>
-    <span id="expirationdate_day-<?php
-    echo $id; ?>" style="display: none;"><?php
-        echo $day; ?></span>
-    <span id="expirationdate_hour-<?php
-    echo $id; ?>" style="display: none;"><?php
-        echo $hour; ?></span>
-    <span id="expirationdate_minute-<?php
-    echo $id; ?>" style="display: none;"><?php
-        echo $minute; ?></span>
-    <span id="expirationdate_enabled-<?php
-    echo $id; ?>" style="display: none;"><?php
-        echo $enabled; ?></span>
-    <span id="expirationdate_expireType-<?php
-    echo $id; ?>" style="display: none;"><?php
-        echo $expireType; ?></span>
-    <span id="expirationdate_categories-<?php
-    echo $id; ?>" style="display: none;"><?php
-        echo $categories; ?></span>
+    <?php echo esc_html($display); ?>
+    <span id="expirationdate_year-<?php echo $id; ?>" style="display: none;"><?php echo $year; ?></span>
+    <span id="expirationdate_month-<?php echo $id; ?>" style="display: none;"><?php echo $month; ?></span>
+    <span id="expirationdate_day-<?php echo $id; ?>" style="display: none;"><?php echo $day; ?></span>
+    <span id="expirationdate_hour-<?php echo $id; ?>" style="display: none;"><?php echo $hour; ?></span>
+    <span id="expirationdate_minute-<?php echo $id; ?>" style="display: none;"><?php echo $minute; ?></span>
+    <span id="expirationdate_enabled-<?php echo $id; ?>" style="display: none;"><?php echo $enabled; ?></span>
+    <span id="expirationdate_expireType-<?php echo $id; ?>" style="display: none;"><?php echo $expireType; ?></span>
+    <span id="expirationdate_categories-<?php echo $id; ?>" style="display: none;"><?php echo $categories; ?></span>
 </div>
