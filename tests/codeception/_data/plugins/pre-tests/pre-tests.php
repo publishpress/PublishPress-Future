6962f360c5f09a26e758289172cf5587eae04c27
andergmartins <anderson@publishpress.com>
2022-05-31 18:42:19 -0300

    Fix markup of the taxonomy field in the Post Types defaults settings, #144

--- tests/codeception/_data/plugins/pre-tests/pre-tests.php
+++ tests/codeception/_data/plugins/pre-tests/pre-tests.php
@@ -9,19 +9,49 @@
  * Text Domain: pre-tests
  */
 
+namespace PreTests;
 
-add_action(
-    'init',
-    function () {
-        register_post_type(
-            'music',
-            [
-                'label'    => 'Musics',
-                'supports' => ['title'],
-                'public'   => true,
-                'show_ui'  => true,
+add_action('init', 'PreTests\registerPostTypes', 1);
+add_action('init', 'PreTests\registerTaxonomies', 1);
+
+function registerPostTypes()
+{
+    register_post_type(
+        'music',
+        [
+            'label'    => 'Musics',
+            'supports' => ['title'],
+            'public'   => true,
+            'show_ui'  => true,
+        ]
+    );
+}
+
+function registerTaxonomies()
+{
+    register_taxonomy(
+        'tax1',
+        'post',
+        [
+            'hierarchical' => true,
+            'labels' => [
+                'name' => 'Tax1',
+                'singular_name' => 'tax1',
+                'plural_name' => 'tax1s'
             ]
-        );
-    },
-    1
-);
+        ]
+    );
+
+    register_taxonomy(
+        'tax2',
+        'post',
+        [
+            'hierarchical' => true,
+            'labels' => [
+                'name' => 'Tax2',
+                'singular_name' => 'tax2',
+                'plural_name' => 'tax2s'
+            ]
+        ]
+    );
+}
--- tests/codeception/acceptanceGutenberg/Features/Admin/settings.feature
+++ tests/codeception/acceptanceGutenberg/Features/Admin/settings.feature
@@ -1,7 +1,7 @@
 Feature: Settings
   In order to configure the plugin
   As an admin
-  I need to be able to see the admin menu
+  I need to be able to see and use the admin menu
 
   Background:
     Given the user "admin_user" exists with role "administrator"
@@ -11,3 +11,9 @@ Feature: Settings
   Scenario: See the Future admin menu
     When I am on the admin home page
     Then I see the Future admin menu on the sidebar
+
+  Scenario: Change Post default expiration taxonomy
+    Given I am on the settings page in the Post Types tab
+    When I change the default taxonomy to "tax1" for "Post"
+    And I save the changes
+    Then I see the taxonomy "tax1" as the default one for "Post"
