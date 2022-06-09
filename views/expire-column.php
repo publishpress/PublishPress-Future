8d6ba0a84055bc894e7c37df0fb387cc547728c2
andergmartins <anderson@publishpress.com>
2022-06-09 15:00:41 -0300

    Rename vars to fix VIP warning Warning about overriding global vars

--- views/expire-column.php
+++ views/expire-column.php
@@ -34,22 +34,22 @@ defined('ABSPATH') or die('Direct access not allowed.');
     // these defaults will be used by quick edit
     $defaults = PostExpirator_Facade::get_default_expiry($post_type);
 
-    $year = $defaults['year'];
-    $month = $defaults['month'];
-    $day = $defaults['day'];
-    $hour = $defaults['hour'];
-    $minute = $defaults['minute'];
+    $defaultYear = $defaults['year'];
+    $defaultMonth = $defaults['month'];
+    $defaultDay = $defaults['day'];
+    $defaultHour = $defaults['hour'];
+    $defaultMinute = $defaults['minute'];
     $enabled = $expirationDate && $expirationEnabled ? 'true' : 'false';
     $categories = '';
 
     // Values for Quick Edit
     if ($expirationDate) {
         $date = gmdate('Y-m-d H:i:s', $expirationDate);
-        $year = get_date_from_gmt($date, 'Y');
-        $month = get_date_from_gmt($date, 'm');
-        $day = get_date_from_gmt($date, 'd');
-        $hour = get_date_from_gmt($date, 'H');
-        $minute = get_date_from_gmt($date, 'i');
+        $defaultYear = get_date_from_gmt($date, 'Y');
+        $defaultMonth = get_date_from_gmt($date, 'm');
+        $defaultDay = get_date_from_gmt($date, 'd');
+        $defaultHour = get_date_from_gmt($date, 'H');
+        $defaultMinute = get_date_from_gmt($date, 'i');
         if (isset($attributes['expireType'])) {
             $expireType = $attributes['expireType'];
         }
@@ -69,11 +69,11 @@ defined('ABSPATH') or die('Direct access not allowed.');
     <span class="dashicons dashicons-<?php echo esc_attr($iconClass); ?>" title="<?php echo esc_attr($iconTitle); ?>"></span>
 
     <?php echo esc_html($display); ?>
-    <input type="hidden" id="expirationdate_year-<?php echo esc_attr($id); ?>" value="<?php echo esc_attr($year); ?>" />
-    <input type="hidden" id="expirationdate_month-<?php echo esc_attr($id); ?>" value="<?php echo esc_attr($month); ?>" />
-    <input type="hidden" id="expirationdate_day-<?php echo esc_attr($id); ?>" value="<?php echo esc_attr($day); ?>" />
-    <input type="hidden" id="expirationdate_hour-<?php echo esc_attr($id); ?>" value="<?php echo esc_attr($hour); ?>" />
-    <input type="hidden" id="expirationdate_minute-<?php echo esc_attr($id); ?>" value="<?php echo esc_attr($minute); ?>" />
+    <input type="hidden" id="expirationdate_year-<?php echo esc_attr($id); ?>" value="<?php echo esc_attr($defaultYear); ?>" />
+    <input type="hidden" id="expirationdate_month-<?php echo esc_attr($id); ?>" value="<?php echo esc_attr($defaultMonth); ?>" />
+    <input type="hidden" id="expirationdate_day-<?php echo esc_attr($id); ?>" value="<?php echo esc_attr($defaultDay); ?>" />
+    <input type="hidden" id="expirationdate_hour-<?php echo esc_attr($id); ?>" value="<?php echo esc_attr($defaultHour); ?>" />
+    <input type="hidden" id="expirationdate_minute-<?php echo esc_attr($id); ?>" value="<?php echo esc_attr($defaultMinute); ?>" />
     <input type="hidden" id="expirationdate_enabled-<?php echo esc_attr($id); ?>" value="<?php echo esc_attr($enabled); ?>" />
     <input type="hidden" id="expirationdate_expireType-<?php echo esc_attr($id); ?>" value="<?php echo esc_attr($expireType); ?>" />
     <input type="hidden" id="expirationdate_categories-<?php echo esc_attr($id); ?>" value="<?php echo esc_attr($categories); ?>" />
