646f1ac33e81e969eaba593fb94fcf5c7caa8f5b
andergmartins <anderson@publishpress.com>
2022-06-16 22:28:38 -0300

    Simplifies the copy bootstrap config

--- bin/docker/scripts/build.sh
+++ bin/docker/scripts/build.sh
@@ -2,13 +2,19 @@
 COMMAND=$1
 SOURCE_PATH=$(pwd)
 PATH_PHP_SCRIPTS=$SOURCE_PATH/bin/docker/scripts
+DIST_PATH=$SOURCE_PATH/dist
 
 get_plugin_name() {
     php $PATH_PHP_SCRIPTS/parse-json.php $SOURCE_PATH/composer.json name | awk -F/ '{print $NF}'
 }
 
+get_plugin_version() {
+    cat post-expirator.php | grep "* Version:" | sed 's/ //g' | awk -F: '{print $NF}'
+}
+
 PLUGIN_NAME=$(get_plugin_name)
-TMP_BUILD_DIR=$SOURCE_PATH/dist/$PLUGIN_NAME
+PLUGIN_VERSION=$(get_plugin_version)
+TMP_BUILD_DIR=$DIST_PATH/$PLUGIN_NAME
 
 clean_dist() {
     rm -rf $TMP_BUILD_DIR
@@ -22,16 +28,21 @@ build_to_dir() {
     rm -rf $TMP_BUILD_DIR/composer.json $TMP_BUILD_DIR/composer.lock
 }
 
-get_plugin_version() {
-
+pack_built_dir() {
+    ZIP_PATH=$DIST_PATH/$PLUGIN_NAME-$PLUGIN_VERSION.zip
+    rm -f $ZIP_PATH
+    pushd $DIST_PATH
+    zip -qr $ZIP_PATH ./$PLUGIN_NAME
+    popd
 }
-
-echo $(get_plugin_version)
-exit 0
 
 case $COMMAND in
     "build-dir")
         build_to_dir
+        ;;
+    "build")
+        build_to_dir
+        pack_built_dir
         ;;
     "clean")
         clean_dist
