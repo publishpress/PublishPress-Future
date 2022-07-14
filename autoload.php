f19dbfd286626bed2b76405bf0219ffdae7ae48e
Anderson G. Martins <anderson@publishpress.com>
2022-07-14 19:39:46 -0300

    Move the autoload functions to a specific file

---
+++ autoload.php
@@ -0,0 +1,25 @@
+<?php
+
+
+/**
+ * Autoloads the classes.
+ */
+function postexpirator_autoload($class)
+{
+    $namespaces = array('PostExpirator');
+    foreach ($namespaces as $namespace) {
+        if (substr($class, 0, strlen($namespace)) === $namespace) {
+            $class = str_replace('_', '', strstr($class, '_'));
+            $filename = POSTEXPIRATOR_BASEDIR . '/classes/' . sprintf('%s.class.php', $class);
+            if (is_readable($filename)) {
+                require_once $filename;
+
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+spl_autoload_register('postexpirator_autoload');
--- post-expirator.php
+++ post-expirator.php
@@ -52,28 +52,7 @@ if (! defined('POSTEXPIRATOR_LOADED')) {
         include_once $autoloadPath;
     }
 
-    /**
-     * Autoloads the classes.
-     */
-    function postexpirator_autoload($class)
-    {
-        $namespaces = array('PostExpirator');
-        foreach ($namespaces as $namespace) {
-            if (substr($class, 0, strlen($namespace)) === $namespace) {
-                $class = str_replace('_', '', strstr($class, '_'));
-                $filename = POSTEXPIRATOR_BASEDIR . '/classes/' . sprintf('%s.class.php', $class);
-                if (is_readable($filename)) {
-                    require_once $filename;
-
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
-
-    spl_autoload_register('postexpirator_autoload');
+    require_once POSTEXPIRATOR_BASEDIR . '/autoload.php';
 
     /**
      * Launch the plugin by initializing its helpers.
