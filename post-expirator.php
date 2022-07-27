fb7cfaf73b552bd7bb3b10f0655c902a804b0e3c
Anderson G. Martins <anderson@publishpress.com>
2022-07-27 11:17:04 -0300

    Expand exception handling in the main file

--- post-expirator.php
+++ post-expirator.php
@@ -16,17 +16,17 @@ use PublishPressFuture\Framework\Dependencies\ServicesAbstract;
 if (! defined('PUBLISHPRESS_FUTURE_LOADED')) {
     define('PUBLISHPRESS_FUTURE_LOADED', true);
 
-    $autoloadPath = __DIR__ . '/vendor/autoload.php';
-    if (! class_exists('PublishPressFuture\\Core\\PluginFacade') && is_readable($autoloadPath)) {
-        require_once $autoloadPath;
-    }
-
-    $services = require __DIR__ . '/services.php';
-    $container = new Container($services);
-
-    $pluginFile = __FILE__;
-
     try {
+        $autoloadPath = __DIR__ . '/vendor/autoload.php';
+        if (! class_exists('PublishPressFuture\\Core\\PluginFacade') && is_readable($autoloadPath)) {
+            require_once $autoloadPath;
+        }
+
+        $services = require __DIR__ . '/services.php';
+        $container = new Container($services);
+
+        $pluginFile = __FILE__;
+
         require_once __DIR__ . '/legacy/defines.php';
         require_once __DIR__ . '/legacy/functions.php';
         require_once __DIR__ . '/legacy/autoload.php';
