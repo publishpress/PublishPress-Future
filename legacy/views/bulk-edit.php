45213fdb6f0ef0f62d33cfc2b459978c6a4366a9
Anderson G. Martins <anderson@publishpress.com>
2022-08-12 13:25:51 -0300

    Refactor changing "Category" to a more generic term "Taxonomy".

--- legacy/views/bulk-edit.php
+++ legacy/views/bulk-edit.php
@@ -125,7 +125,7 @@ $minute = $defaults['minute'];
                             <span class="title"><?php
                                 echo esc_html($tax_label); ?></span>
                             <span class="screen-reader-text"><?php
-                                esc_html_e('Expiration Categories', 'post-expirator'); ?></span>
+                                esc_html_e('Expiration Taxonomies', 'post-expirator'); ?></span>
                         </legend>
                         <ul id="categorychecklist"
                             class="list:category categorychecklist cat-checklist category-checklist">
