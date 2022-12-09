3b52837a26a4e4a0641ff98c103a7b5b7e20c8a3
Anderson Grüdtner Martins <anderson@publishpress.com>
2022-12-09 17:01:31 -0300

    Fix the path for the language files

--- legacy/functions.php
+++ legacy/functions.php
@@ -38,7 +38,8 @@ add_filter('plugin_action_links', 'postexpirator_plugin_action_links', 10, 2);
  */
 function postexpirator_init()
 {
-    $plugin_dir = plugin_basename(__DIR__);
+    $container = \PublishPressFuture\Core\DI\Container::getInstance();
+    $plugin_dir = basename($container->get(ServicesAbstract::BASE_PATH));
     load_plugin_textdomain('post-expirator', null, $plugin_dir . '/languages/');
 
     PostExpirator_Reviews::init();
