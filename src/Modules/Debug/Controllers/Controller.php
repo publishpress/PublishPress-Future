132f1b400f0b8d6d9382cff857c5441b297cb5bb
Anderson Martins <anderson@publishpress.com>
2023-02-07 20:37:52 -0300

    Fix option to drop debug table
    If setting is configured to not preserve data, we should drop the debug table.

--- src/Modules/Debug/Controllers/Controller.php
+++ src/Modules/Debug/Controllers/Controller.php
@@ -5,6 +5,7 @@
 
 namespace PublishPressFuture\Modules\Debug\Controllers;
 
+use PublishPressFuture\Core\HooksAbstract as CoreAbstractHooks;
 use PublishPressFuture\Framework\InitializableInterface;
 use PublishPressFuture\Framework\Logger\LoggerInterface;
 use PublishPressFuture\Framework\WordPress\Facade\HooksFacade;
@@ -31,10 +32,24 @@ class Controller implements InitializableInterface
     public function initialize()
     {
         $this->hooks->addAction(HooksAbstract::ACTION_DEBUG_LOG, [$this, 'onActionDebugLog']);
+
+        $this->hooks->addAction(
+            CoreAbstractHooks::ACTION_DEACTIVATE_PLUGIN,
+            [$this, 'onActionDeactivatePlugin']
+        );
     }
 
     public function onActionDebugLog($message)
     {
         $this->logger->debug($message);
+    }
+
+    public function onActionDeactivatePlugin()
+    {
+        $preserveData = (bool)get_option('expirationdatePreserveData', 1);
+
+        if (! $preserveData) {
+            $this->logger->dropDatabaseTable();
+        }
     }
 }
