21a09ce8247134cc9384f8b347dcaa02c6f01b9d
Anderson Martins <anderson@publishpress.com>
2023-02-16 13:57:07 -0300

    Prepare webpack for compiling jsx files

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var glob = require("glob");
+var path = require("path");
+
+module.exports = [
+    {
+        entry: glob.sync(
+            "./src/assets/jsx/settings/*.jsx",
+        ),
+        devtool: 'source-map',
+        output: {
+            path: path.join(__dirname, "src", "assets", "js"),
+            filename: "settings.js"
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(jsx)$/, // Identifies which file or files should be transformed.
+                    use: {loader: "babel-loader"}, // Babel loader to transpile modern JavaScript.
+                    exclude: [
+                        /(node_modules|bower_components)/,
+                    ]// JavaScript files to be ignored.
+                }
+            ]
+        }
+    }
+];
