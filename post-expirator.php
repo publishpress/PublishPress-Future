b799b0b8c884adad547dcb09db618b5e6b1b5d68
Anderson Martins <anderson@publishpress.com>
2023-06-08 15:20:50 -0300

    Catch exceptions in the main plugin context and log as error

--- post-expirator.php
+++ post-expirator.php
@@ -16,9 +16,13 @@ use PublishPress\Future\Core\Autoloader;
 use PublishPress\Future\Core\DI\Container;
 use PublishPress\Future\Core\DI\ServicesAbstract;
 
+use function PublishPress\Future\logCatchException;
+
 defined('ABSPATH') or die('Direct access not allowed.');
 
 if (! defined('PUBLISHPRESS_FUTURE_LOADED')) {
+    include __DIR__ . '/src/catch-exception.php';
+
     try {
         define('PUBLISHPRESS_FUTURE_LOADED', true);
 
@@ -34,44 +38,29 @@ if (! defined('PUBLISHPRESS_FUTURE_LOADED')) {
         require_once PUBLISHPRESS_FUTURE_VENDOR_PATH . '/woocommerce/action-scheduler/action-scheduler.php';
 
         add_action('plugins_loaded', function () {
-            if (! class_exists('PublishPress\Future\Core\Autoloader')) {
-                require_once __DIR__ . '/src/Core/Autoloader.php';
+            try {
+                if (! class_exists('PublishPress\Future\Core\Autoloader')) {
+                    require_once __DIR__ . '/src/Core/Autoloader.php';
+                }
+                Autoloader::register();
+
+                $pluginFile = __FILE__;
+
+                $services = require __DIR__ . '/services.php';
+                $container = new Container($services);
+
+                require_once __DIR__ . '/legacy/defines.php';
+                require_once __DIR__ . '/legacy/deprecated.php';
+                require_once __DIR__ . '/legacy/functions.php';
+                require_once __DIR__ . '/legacy/autoload.php';
+
+                $container->get(ServicesAbstract::PLUGIN)->initialize();
+            } catch (Exception $e) {
+                logCatchException($e);
             }
-            Autoloader::register();
-
-            $pluginFile = __FILE__;
-
-            $services = require __DIR__ . '/services.php';
-            $container = new Container($services);
-
-            require_once __DIR__ . '/legacy/defines.php';
-            require_once __DIR__ . '/legacy/deprecated.php';
-            require_once __DIR__ . '/legacy/functions.php';
-            require_once __DIR__ . '/legacy/autoload.php';
-
-            $container->get(ServicesAbstract::PLUGIN)->initialize();
         }, 10, 0);
 
     } catch (Exception $e) {
-        $trace = $e->getTrace();
-
-        $traceText = '';
-
-        foreach ($trace as $item) {
-            $traceText .= $item['file'] . ':' . $item['line'] . ' ' . $item['function'] . '(), ';
-        }
-
-        $message = sprintf(
-            "PUBLISHPRESS FUTURE Exception: %s: %s. Backtrace: %s",
-            get_class($e),
-            $e->getMessage(),
-            $traceText
-        );
-
-        // Make the log message binary safe removing any non-printable chars.
-        $message = addcslashes($message, "\000..\037\177..\377\\");
-
-        // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
-        error_log($message);
+        logCatchException($e);
     }
 }
