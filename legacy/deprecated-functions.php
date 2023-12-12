cc4de202a4e9c2acd5b6ed2530aea598b6954ac7
Anderson Martins <anderson@publishpress.com>
2023-12-12 14:52:23 -0300

    Add deprecated warnings and function_exists conditional to functions

--- legacy/deprecated-functions.php
+++ legacy/deprecated-functions.php
@@ -108,289 +108,331 @@ if (! function_exists('expirationdate_get_blog_url')) {
     }
 }
 
-/**
- * Schedules the single event.
- *
- * @deprecated 2.8.0
- * @internal
- *
- * @access private
- */
-function postexpirator_schedule_event($postId, $timestamp, $opts)
-{
-    do_action(ExpiratorHooks::ACTION_SCHEDULE_POST_EXPIRATION, $postId, $timestamp, $opts);
+if (! function_exists('postexpirator_schedule_event')) {
+    /**
+     * Schedules the single event.
+     *
+     * @deprecated 2.8.0
+     * @internal
+     *
+     * @access private
+     */
+    function postexpirator_schedule_event($postId, $timestamp, $opts)
+    {
+        _deprecated_function(__FUNCTION__, '2.8.0', 'Use the PublishPress\Future\Modules\Expirator\HooksAbstract::ACTION_SCHEDULE_POST_EXPIRATION hook instead');
+
+        do_action(ExpiratorHooks::ACTION_SCHEDULE_POST_EXPIRATION, $postId, $timestamp, $opts);
+    }
 }
 
-/**
- * Unschedules the single event.
- *
- * @internal
- *
- * @access private
- * @deprecated 2.8.0
- */
-function postexpirator_unschedule_event($postId)
-{
-    do_action(ExpiratorHooks::ACTION_UNSCHEDULE_POST_EXPIRATION, $postId);
+if (! function_exists('postexpirator_unschedule_event')) {
+    /**
+     * Unschedules the single event.
+     *
+     * @internal
+     *
+     * @access private
+     * @deprecated 2.8.0
+     */
+    function postexpirator_unschedule_event($postId)
+    {
+        _deprecated_function(__FUNCTION__, '2.8.0', 'Use the PublishPress\Future\Modules\Expirator\HooksAbstract::ACTION_UNSCHEDULE_POST_EXPIRATION hook instead');
+
+        do_action(ExpiratorHooks::ACTION_UNSCHEDULE_POST_EXPIRATION, $postId);
+    }
 }
 
-
-/**
- * Show the menu.
- *
- * @internal
- *
- * @access private
- * @deprecated 2.5.0
- */
-function postexpirator_menu()
-{
-    _deprecated_function(__FUNCTION__, '2.5.0');
+if (! function_exists('postexpirator_menu')) {
+    /**
+     * Show the menu.
+     *
+     * @internal
+     *
+     * @access private
+     * @deprecated 2.5.0
+     */
+    function postexpirator_menu()
+    {
+        _deprecated_function(__FUNCTION__, '2.5.0');
+    }
 }
 
-/**
- * Hook's to add plugin page menu
- *
- * @internal
- *
- * @access private
- * @deprecated 2.5.0
- */
-function postexpirator_add_menu()
-{
-    _deprecated_function(__FUNCTION__, '2.5.0');
+if (! function_exists('postexpirator_add_menu')) {
+    /**
+     * Hook's to add plugin page menu
+     *
+     * @internal
+     *
+     * @access private
+     * @deprecated 2.5.0
+     */
+    function postexpirator_add_menu()
+    {
+        _deprecated_function(__FUNCTION__, '2.5.0');
+    }
 }
 
-/**
- * Show the Expiration Date options page
- *
- * @internal
- *
- * @access private
- * @deprecated 2.5.0
- */
-function postexpirator_menu_general()
-{
-    _deprecated_function(__FUNCTION__, '2.5.0');
-    PostExpirator_Display::getInstance()->load_tab('general');
+if (! function_exists('postexpirator_menu_general')) {
+    /**
+     * Show the Expiration Date options page
+     *
+     * @internal
+     *
+     * @access private
+     * @deprecated 2.5.0
+     */
+    function postexpirator_menu_general()
+    {
+        _deprecated_function(__FUNCTION__, '2.5.0');
+
+        PostExpirator_Display::getInstance()->load_tab('general');
+    }
 }
 
-/**
- * The default menu.
- *
- * @internal
- *
- * @access private
- * @deprecated 2.5.0
- */
-function postexpirator_menu_defaults()
-{
-    _deprecated_function(__FUNCTION__, '2.5.0');
-    PostExpirator_Display::getInstance()->load_tab('defaults');
+if (! function_exists('postexpirator_menu_defaults')) {
+    /**
+     * The default menu.
+     *
+     * @internal
+     *
+     * @access private
+     * @deprecated 2.5.0
+     */
+    function postexpirator_menu_defaults()
+    {
+        _deprecated_function(__FUNCTION__, '2.5.0');
+
+        PostExpirator_Display::getInstance()->load_tab('defaults');
+    }
 }
 
-/**
- * Diagnostics menu.
- *
- * @internal
- *
- * @access private
- * @deprecated 2.5.0
- */
-function postexpirator_menu_diagnostics()
-{
-    _deprecated_function(__FUNCTION__, '2.5.0');
-    PostExpirator_Display::getInstance()->load_tab('diagnostics');
+if (! function_exists('postexpirator_menu_diagnostics')) {
+    /**
+     * Diagnostics menu.
+     *
+     * @internal
+     *
+     * @access private
+     * @deprecated 2.5.0
+     */
+    function postexpirator_menu_diagnostics()
+    {
+        _deprecated_function(__FUNCTION__, '2.5.0');
+
+        PostExpirator_Display::getInstance()->load_tab('diagnostics');
+    }
 }
 
-/**
- * Debug menu.
- *
- * @internal
- *
- * @access private
- * @deprecated 2.5.0
- */
-function postexpirator_menu_debug()
-{
-    _deprecated_function(__FUNCTION__, '2.5.0');
-    PostExpirator_Display::getInstance()->load_tab('viewdebug');
+if (! function_exists('postexpirator_menu_debug')) {
+    /**
+     * Debug menu.
+     *
+     * @internal
+     *
+     * @access private
+     * @deprecated 2.5.0
+     */
+    function postexpirator_menu_debug()
+    {
+        _deprecated_function(__FUNCTION__, '2.5.0');
+
+        PostExpirator_Display::getInstance()->load_tab('viewdebug');
+    }
 }
 
-/**
- * Check for Debug
- *
- * @internal
- *
- * @access private
- * @deprecated 2.8.0
- */
-function postexpirator_debug()
-{
-    _deprecated_function(__FUNCTION__, '2.8.0');
+if (! function_exists('postexpirator_debug')) {
+    /**
+     * Check for Debug
+     *
+     * @internal
+     *
+     * @access private
+     * @deprecated 2.8.0
+     */
+    function postexpirator_debug()
+    {
+        _deprecated_function(__FUNCTION__, '2.8.0');
 
-    $debug = get_option('expirationdateDebug');
+        $debug = get_option('expirationdateDebug');
 
-    // phpcs:ignore WordPress.PHP.StrictComparisons.LooseComparison
-    if ($debug == 1) {
-        if (! defined('POSTEXPIRATOR_DEBUG')) {
-            define('POSTEXPIRATOR_DEBUG', 1);
+        // phpcs:ignore WordPress.PHP.StrictComparisons.LooseComparison
+        if ($debug == 1) {
+            if (! defined('POSTEXPIRATOR_DEBUG')) {
+                define('POSTEXPIRATOR_DEBUG', 1);
+            }
+
+            require_once POSTEXPIRATOR_LEGACYDIR . '/debug.php';
+
+            return new PostExpiratorDebug();
+        } else {
+            if (! defined('POSTEXPIRATOR_DEBUG')) {
+                /**
+                 * @deprecated
+                 */
+                define('POSTEXPIRATOR_DEBUG', 0);
+            }
+
+            return false;
+        }
+    }
+}
+
+if (! function_exists('_postexpirator_get_cat_names')) {
+    /**
+     * Internal method to get category names corresponding to the category IDs.
+     *
+     * @internal
+     *
+     * @access private
+     * @deprecated 2.8.0
+     */
+    function _postexpirator_get_cat_names($cats)
+    {
+        _deprecated_function(__FUNCTION__, '2.8.0');
+
+        $out = array();
+        foreach ($cats as $cat) {
+            $out[$cat] = get_the_category_by_id($cat);
         }
 
-        require_once POSTEXPIRATOR_LEGACYDIR . '/debug.php';
+        return $out;
+    }
+}
 
-        return new PostExpiratorDebug();
-    } else {
-        if (! defined('POSTEXPIRATOR_DEBUG')) {
-            /**
-             * @deprecated
-             */
-            define('POSTEXPIRATOR_DEBUG', 0);
+if (! function_exists('postexpirator_register_expiration_meta')) {
+    /**
+     * @param $id
+     * @param $log
+     * @return void
+     * @deprecated 2.8.0
+     */
+    function postexpirator_register_expiration_meta($id, $log)
+    {
+        _deprecated_function(__FUNCTION__, '2.8.0');
+
+        // phpcs:ignore WordPress.DateTime.RestrictedFunctions.date_date
+        $log['expired_on'] = date('Y-m-d H:i:s');
+
+        add_post_meta($id, 'expiration_log', wp_json_encode($log));
+    }
+}
+
+if (! function_exists('postexpirator_expire_post')) {
+    /**
+     * The new expiration function, to work with single scheduled events.
+     *
+     * This was designed to hopefully be more flexible for future tweaks/modifications to the architecture.
+     *
+     * @internal
+     *
+     * @access private
+     * @deprecated 2.8.0
+     */
+    function postexpirator_expire_post($postId)
+    {
+        _deprecated_function(__FUNCTION__, '2.8.0');
+
+        do_action(ExpiratorHooks::ACTION_RUN_WORKFLOW, $postId);
+    }
+}
+
+if (! function_exists('postexpirator_activate')) {
+    /**
+     * Called at plugin activation
+     *
+     * @internal
+     *
+     * @access private
+     * @deprecated 2.8.0
+     */
+    function postexpirator_activate()
+    {
+        _deprecated_function(__METHOD__, '2.8.0', 'Moved to the module Settings');
+    }
+}
+
+if (! function_exists('expirationdate_deactivate')) {
+    /**
+     * Called at plugin deactivation
+     *
+     * @internal
+     *
+     * @access private
+     *
+     * @deprecated 2.8.0
+     */
+    function expirationdate_deactivate()
+    {
+        _deprecated_function(__METHOD__, '2.8.0', 'Moved to the PublishPress\Future\Framework\PluginFacade class.');
+    }
+}
+
+if (! function_exists('_postexpirator_taxonomy')) {
+
+    /**
+     * Get the HTML for taxonomy.
+     *
+     * @internal
+     *
+     * @access private
+     * @deprecated 3.1.4
+     */
+    function _postexpirator_taxonomy($opts)
+    {
+        _deprecated_function(__FUNCTION__, '3.1.4');
+
+        if (empty($opts)) {
+            return false;
         }
 
-        return false;
+        if (! isset($opts['name'])) {
+            return false;
+        }
+
+        $name = sanitize_text_field($opts['name']);
+
+        if (! isset($id)) {
+            $id = $name;
+        }
+
+        $disabled = false;
+        if (isset($opts['disabled'])) {
+            $disabled = (bool)$opts['disabled'];
+        }
+
+        $onchange = '';
+        if (isset($opts['onchange'])) {
+            $onchange = sanitize_text_field($opts['onchange']);
+        }
+
+        $type = '';
+        if (isset($opts['type'])) {
+            $type = sanitize_text_field($opts['type']);
+        }
+
+        $selected = false;
+        if (isset($opts['selected'])) {
+            $selected = $opts['selected'];
+        }
+
+        $taxonomies = get_object_taxonomies($type, 'object');
+        $taxonomies = wp_filter_object_list($taxonomies, array('hierarchical' => true));
+
+        if (empty($taxonomies)) {
+            return esc_html__('No taxonomies found', 'post-expirator');
+        }
+
+        $params = [
+            'name' => $name,
+            'id' => $id,
+            'disabled' => $disabled,
+            'taxonomies' => $taxonomies,
+            'selected' => $selected,
+            'onchange' => $onchange
+        ];
+
+        return PostExpirator_Display::getInstance()->get_rendered_template('taxonomy-field', $params);
     }
-}
-
-/**
- * Internal method to get category names corresponding to the category IDs.
- *
- * @internal
- *
- * @access private
- * @deprecated 2.8.0
- */
-function _postexpirator_get_cat_names($cats)
-{
-    $out = array();
-    foreach ($cats as $cat) {
-        $out[$cat] = get_the_category_by_id($cat);
-    }
-
-    return $out;
-}
-
-/**
- * @param $id
- * @param $log
- * @return void
- * @deprecated 2.8.0
- */
-function postexpirator_register_expiration_meta($id, $log)
-{
-    // phpcs:ignore WordPress.DateTime.RestrictedFunctions.date_date
-    $log['expired_on'] = date('Y-m-d H:i:s');
-
-    add_post_meta($id, 'expiration_log', wp_json_encode($log));
-}
-
-/**
- * The new expiration function, to work with single scheduled events.
- *
- * This was designed to hopefully be more flexible for future tweaks/modifications to the architecture.
- *
- * @internal
- *
- * @access private
- * @deprecated 2.8.0
- */
-function postexpirator_expire_post($postId)
-{
-    do_action(
-        ExpiratorHooks::ACTION_RUN_WORKFLOW,
-        $postId
-    );
-}
-
-/**
- * Called at plugin activation
- *
- * @internal
- *
- * @access private
- * @deprecated 2.8.0
- */
-function postexpirator_activate()
-{
-    _deprecated_function(__METHOD__, '2.8.0', 'Moved to the module Settings');
-}
-
-/**
- * Called at plugin deactivation
- *
- * @internal
- *
- * @access private
- *
- * @deprecated 2.8.0
- */
-function expirationdate_deactivate()
-{
-    _deprecated_function(__METHOD__, '2.8.0', 'Moved to the PublishPress\Future\Framework\PluginFacade class.');
-}
-
-
-/**
- * Get the HTML for taxonomy.
- *
- * @internal
- *
- * @access private
- * @deprecated 3.1.4
- */
-function _postexpirator_taxonomy($opts)
-{
-    if (empty($opts)) {
-        return false;
-    }
-
-    if (! isset($opts['name'])) {
-        return false;
-    }
-
-    $name = sanitize_text_field($opts['name']);
-
-    if (! isset($id)) {
-        $id = $name;
-    }
-
-    $disabled = false;
-    if (isset($opts['disabled'])) {
-        $disabled = (bool)$opts['disabled'];
-    }
-
-    $onchange = '';
-    if (isset($opts['onchange'])) {
-        $onchange = sanitize_text_field($opts['onchange']);
-    }
-
-    $type = '';
-    if (isset($opts['type'])) {
-        $type = sanitize_text_field($opts['type']);
-    }
-
-    $selected = false;
-    if (isset($opts['selected'])) {
-        $selected = $opts['selected'];
-    }
-
-    $taxonomies = get_object_taxonomies($type, 'object');
-    $taxonomies = wp_filter_object_list($taxonomies, array('hierarchical' => true));
-
-    if (empty($taxonomies)) {
-        return esc_html__('No taxonomies found', 'post-expirator');
-    }
-
-    $params = [
-        'name' => $name,
-        'id' => $id,
-        'disabled' => $disabled,
-        'taxonomies' => $taxonomies,
-        'selected' => $selected,
-        'onchange' => $onchange
-    ];
-
-    return PostExpirator_Display::getInstance()->get_rendered_template('taxonomy-field', $params);
 }
 
 if (! function_exists('postexpirator_get_post_types')) {
