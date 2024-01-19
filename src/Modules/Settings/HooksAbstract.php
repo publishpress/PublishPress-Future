d803b2554b7d114c6fe18d9131ac88b2eb71ba23
Anderson Martins <anderson@publishpress.com>
2024-01-19 16:26:48 -0300

    Add new action on saving post type default actions. The new action pass the settings and a list of all post types.

--- src/Modules/Settings/HooksAbstract.php
+++ src/Modules/Settings/HooksAbstract.php
@@ -18,6 +18,7 @@ abstract class HooksAbstract
     const FILTER_SHOW_PRO_BANNER = 'publishpressfuture_show_pro_banner';
     const FILTER_SAVE_DEFAULTS_SETTINGS = 'publishpressfuture_save_defaults_settings';
     const ACTION_SAVE_POST_TYPE_SETTINGS = 'publishpressfuture_saved_post_type_settings';
+    const ACTION_SAVE_ALL_POST_TYPES_SETTINGS = 'publishpressfuture_saved_all_post_types_settings';
     const FILTER_SETTINGS_POST_TYPE = 'publishpress_future_settings_post_type';
     const ACTION_SETTINGS_TAB_ADVANCED_BEFORE = 'publishpress_future/settings_tab_advanced_before';
 }
