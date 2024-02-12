e51d5029e4b0e34af8e51186f7cbdbca6c5d9e36
Anderson Martins <anderson@publishpress.com>
2024-02-12 17:36:27 -0300

    Add basic integration test

---
+++ tests/Integration/Core/DI/PluginTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Core\DI;
+
+use Codeception\Stub\Expected;
+use PublishPress\Future\Core\HooksAbstract;
+use PublishPress\Future\Core\Plugin;
+use PublishPress\Future\Framework\WordPress\Facade\HooksFacade;
+use PublishPress\Future\Framework\WordPress\Facade\NoticeFacade;
+use stdClass;
+
+class PluginTest extends \lucatume\WPBrowser\TestCase\WPTestCase
+{
+    /**
+     * @var \IntegrationTester
+     */
+    protected $tester;
+
+    public function testInitializeTriggersActionInitPlugin() :void
+    {
+        $modules = [];
+
+        global $testInitializeTriggersActionInitPluginActions;
+        $testInitializeTriggersActionInitPluginActions = [];
+
+        $hooksFacade = $this->makeEmpty(
+            HooksFacade::class,
+            [
+                'doAction' => Expected::atLeastOnce(
+                    function ($action) use (&$testInitializeTriggersActionInitPluginActions) {
+                        $testInitializeTriggersActionInitPluginActions[] = $action;
+                    }
+                )
+            ]
+        );
+
+        $legacyPlugin = $this->makeEmpty(stdClass::class);
+        $noticesFacade = $this->makeEmpty(NoticeFacade::class);
+
+        $basePath = '/tmp';
+        $pluginSlug = 'post-expirator';
+
+        $plugin = $this->construct(
+            Plugin::class,
+            [
+                $modules,
+                $legacyPlugin,
+                $hooksFacade,
+                $basePath,
+                $pluginSlug,
+                $noticesFacade
+            ]
+        );
+
+        $plugin->initialize();
+
+        $this->assertTrue(in_array(HooksAbstract::ACTION_INIT_PLUGIN, $testInitializeTriggersActionInitPluginActions));
+    }
+}
