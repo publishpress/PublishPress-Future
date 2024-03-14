17f837417aa0a81cb57f1a8b6038ef1a83dd3abd
Anderson Martins <anderson@publishpress.com>
2024-03-14 12:20:15 -0300

    Upgrade dev dependencies

--- lib/vendor/composer/installed.php
+++ lib/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => 'dev-3.2-branch',
-        'version' => 'dev-3.2-branch',
-        'reference' => 'a213c938ba65a0258d82df8ea22282047a7c63ab',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'da10933038d0ad9ee6438e8743603965854342b2',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => 'dev-3.2-branch',
-            'version' => 'dev-3.2-branch',
-            'reference' => 'a213c938ba65a0258d82df8ea22282047a7c63ab',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'da10933038d0ad9ee6438e8743603965854342b2',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
