714daa28f591f54236b7ff9cb43148623879bda1
Anderson Martins <anderson@publishpress.com>
2024-03-22 14:22:12 -0300

    Move the flow editor to a component

---
+++ src/assets/jsx/workflow-editor/components/FlowEditor.jsx
@@ -0,0 +1,33 @@
+import { useSelect } from "@wordpress/data";
+import { store } from "../store";
+import ReactFlow, { ReactFlowProvider, Background, Controls } from "reactflow";
+
+export const FlowEditor = (props) => {
+    const {
+        nodes,
+        edges
+    } = useSelect((select) => {
+        return {
+            nodes: select(store).getNodes(),
+            edges: select(store).getEdges(),
+        }
+    });
+
+    const proOptions = {
+        // TODO: Change this to true after we start supporting the pro version of ReactFlow.
+        hideAttribution: false,
+    }
+
+    return (
+        <ReactFlow
+            defaultNodes={nodes}
+            defaultEdges={edges}
+            fitView
+            proOptions={proOptions}
+            nodesDraggable={true}
+        >
+            <Background variant="dots" />
+            <Controls />
+        </ReactFlow>
+    );
+}
--- src/assets/jsx/workflow-editor/components/LayoutContent.jsx
+++ src/assets/jsx/workflow-editor/components/LayoutContent.jsx
@@ -1,33 +1,7 @@
-import { useSelect } from "@wordpress/data";
-import { store } from "../store";
-import ReactFlow, { ReactFlowProvider, Background, Controls } from "reactflow";
+import { FlowEditor } from "./FlowEditor";
 
 export const LayoutContent = (props) => {
-    const {
-        nodes,
-        edges
-    } = useSelect((select) => {
-        return {
-            nodes: select(store).getNodes(),
-            edges: select(store).getEdges(),
-        }
-    });
-
-    const proOptions = {
-        // TODO: Change this to true after we start supporting the pro version of ReactFlow.
-        hideAttribution: false,
-    }
-
     return (
-        <ReactFlow
-            defaultNodes={nodes}
-            defaultEdges={edges}
-            fitView
-            proOptions={proOptions}
-            nodesDraggable={true}
-        >
-            <Background variant="dots" />
-            <Controls />
-        </ReactFlow>
+        <FlowEditor />
     );
 }
