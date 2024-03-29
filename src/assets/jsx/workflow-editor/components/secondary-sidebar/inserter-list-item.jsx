553d5dcec820b38cb337a60539e70a672f069dbf
Anderson Martins <anderson@publishpress.com>
2024-03-29 16:19:46 -0300

    Move the isAppleOS to utils

--- src/assets/jsx/workflow-editor/components/secondary-sidebar/inserter-list-item.jsx
+++ src/assets/jsx/workflow-editor/components/secondary-sidebar/inserter-list-item.jsx
@@ -16,22 +16,7 @@ import { ENTER } from '@wordpress/keycodes';
 import NodeIcon from './node-icon';
 import { InserterListboxItem } from './inserter-listbox';
 import InserterDraggableNodes from './inserter-draggable-nodes';
-
-/**
- * Return true if platform is MacOS.
- *
- * @param {Object} _window window object by default; used for DI testing.
- *
- * @return {boolean} True if MacOS; false otherwise.
- */
-function isAppleOS(_window = window) {
-    const { platform } = _window.navigator;
-
-    return (
-        platform.indexOf('Mac') !== -1 ||
-        ['iPad', 'iPhone'].includes(platform)
-    );
-}
+import { isAppleOS } from '../../utils';
 
 function InserterListItem({
     className,
--- src/assets/jsx/workflow-editor/utils.jsx
+++ src/assets/jsx/workflow-editor/utils.jsx
@@ -42,3 +42,19 @@ export function getNodeMenuDefaultClassName(blockName) {
         blockName
     );
 }
+
+/**
+ * Return true if platform is MacOS.
+ *
+ * @param {Object} _window window object by default; used for DI testing.
+ *
+ * @return {boolean} True if MacOS; false otherwise.
+ */
+export function isAppleOS(_window = window) {
+    const { platform } = _window.navigator;
+
+    return (
+        platform.indexOf('Mac') !== -1 ||
+        ['iPad', 'iPhone'].includes(platform)
+    );
+}
