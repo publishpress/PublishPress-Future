481cb63abfa1030ce5acc674a755be0681afd573
Anderson Martins <anderson@publishpress.com>
2024-06-10 16:56:50 -0300

    Update translation files and add ES and FR translations for the editor publishpress/publishpress-future#770

--- dev-workspace/scripts/lang-constants.sh
+++ dev-workspace/scripts/lang-constants.sh
@@ -5,7 +5,7 @@ export LANG_DOMAIN="publishpress-future-pro"
 export LANG_DIR="languages"
 
 # Space separated list of locales that the plugin supports in scripts
-export LANG_LOCALES="pt_BR it_IT"
+export LANG_LOCALES="pt_BR it_IT es_ES fr_FR"
 
 # Space separated list of script handlers that the plugin provides
 export LANG_SCRIPT_HANDLERS="future_workflow_editor_script"
