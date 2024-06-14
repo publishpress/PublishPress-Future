3030b26809edd5564a3e52ac59b0802ba9d78d59
Anderson Martins <anderson@publishpress.com>
2024-06-14 17:27:10 -0300

    Update the promo sidebar text, #774

--- src/Views/ad-banner-right-sidebar.php
+++ src/Views/ad-banner-right-sidebar.php
@@ -20,13 +20,15 @@ defined('ABSPATH') or die('Direct access not allowed.');
                 </p>
                 <ul>
                     <li><?php
-                        echo esc_html__('Set the action date based on the post\'s publishing date', 'post-expirator'); ?></li>
+                        echo esc_html__('Design custom workflows with multiple actions', 'post-expirator'); ?></li>
+                    <li><?php
+                        echo esc_html__('Schedule actions based on the post\'s publishing date', 'post-expirator'); ?></li>
                     <li><?php
                         echo esc_html__('Move posts to custom statuses', 'post-expirator'); ?></li>
                     <li><?php
                         echo esc_html__('Use other plugins to control Future Actions', 'post-expirator'); ?></li>
                     <li><?php
-                        echo esc_html__('Fast, professional support', 'post-expirator'); ?></li>
+                        echo esc_html__('Enjoy fast and professional support', 'post-expirator'); ?></li>
                 </ul>
                 <div class="upgrade-btn">
                     <a href="https://publishpress.com/links/future-sidebar" target="__blank"><?php
