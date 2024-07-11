dfa7e0d31163ebbecda5b4c4528a085b8108513d
Anderson Martins <anderson@publishpress.com>
2024-07-11 13:50:24 -0300

    Remove not used use statement

--- lib/vendor/publishpress/publishpress-future/src/Modules/Expirator/Controllers/QuickEditController.php
+++ lib/vendor/publishpress/publishpress-future/src/Modules/Expirator/Controllers/QuickEditController.php
@@ -6,7 +6,6 @@
 namespace PublishPress\Future\Modules\Expirator\Controllers;
 
 use PostExpirator_Display;
-use PostExpirator_Facade;
 use PublishPress\Future\Core\DI\Container;
 use PublishPress\Future\Core\DI\ServicesAbstract;
 use PublishPress\Future\Core\HookableInterface;
