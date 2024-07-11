4710befd73c6266a882e85919f7fd6fc02c9f9f4
Anderson Martins <anderson@publishpress.com>
2024-07-11 11:17:07 -0300

    Update composer files

--- lib/vendor/composer/installed.php
+++ lib/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => 'dev-main',
-        'version' => 'dev-main',
-        'reference' => 'ff81edb7c561549569010d73e1ead1a0fb159fb1',
+        'pretty_version' => 'dev-3.4-branch',
+        'version' => 'dev-3.4-branch',
+        'reference' => 'f9cb6a9d4167ec66edff16d81b62ea94bd94f286',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => 'dev-main',
-            'version' => 'dev-main',
-            'reference' => 'ff81edb7c561549569010d73e1ead1a0fb159fb1',
+            'pretty_version' => 'dev-3.4-branch',
+            'version' => 'dev-3.4-branch',
+            'reference' => 'f9cb6a9d4167ec66edff16d81b62ea94bd94f286',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
