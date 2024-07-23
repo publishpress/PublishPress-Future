981aee00f5d7f3709388d2067e29298dae789f53
Anderson Martins <anderson@publishpress.com>
2024-07-23 18:23:03 -0300

    Update composer scripts

--- lib/vendor/composer/installed.php
+++ lib/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-develop',
         'version' => 'dev-develop',
-        'reference' => '6c6cf412d9abfcc779e15bf84edb39a722a00d5a',
+        'reference' => 'a0b3901ae99c8109782f736b4980c1954d680878',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-develop',
             'version' => 'dev-develop',
-            'reference' => '6c6cf412d9abfcc779e15bf84edb39a722a00d5a',
+            'reference' => 'a0b3901ae99c8109782f736b4980c1954d680878',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
