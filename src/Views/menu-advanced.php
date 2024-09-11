1f131783bba1ee40981c0873a0ec3b9ab8256629
Anderson Martins <anderson@publishpress.com>
2024-09-11 11:18:58 -0300

    Add the setting for enabling experimental features

--- src/Views/menu-advanced.php
+++ src/Views/menu-advanced.php
@@ -77,6 +77,31 @@ $settingsFacade = $container->get(ServicesAbstract::SETTINGS);
                     <!-- React component -->
                 </tr>
 
+                <!-- Enable experimental features -->
+                <?php if (PUBLISHPRESS_FUTURE_WORKFLOW_EXPERIMENTAL) : ?>
+                    <tr valign="top">
+                        <th scope="row">
+                            <?php esc_html_e('Experimental Features', 'publishpress-future-pro'); ?>
+                        </th>
+                        <td>
+                            <div class="pp-settings-field-row">
+                                <input type="checkbox" name="future-experimental-features"
+                                        id="future-experimental-features"
+                                        value="1"
+                                        <?php echo $settingsFacade->getExperimentalFeaturesStatus() ? 'checked' : ''; ?> />
+                                <label for="future-experimental-features"><?php
+                                    esc_html_e('Enable experimental features', 'publishpress-future-pro'); ?></label>
+                                <p class="description offset">
+                                    <?php esc_html_e(
+                                        'Enable experimental features that are still in development and may not be fully functional.',
+                                        'publishpress-future-pro'
+                                    ); ?>
+                                </p>
+                            </div>
+                        </td>
+                    </tr>
+                <?php endif; ?>
+
                 <tr valign="top">
                     <th scope="row"><?php
                         esc_html_e('Future Action Column Style', 'post-expirator'); ?></th>
