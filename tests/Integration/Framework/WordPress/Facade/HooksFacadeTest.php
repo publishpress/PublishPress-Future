a33ac2bae4c1a47a1e3f6d5e64e8538c9ccc1276
Anderson Martins <anderson@publishpress.com>
2024-10-31 14:52:37 -0300

    Minor refactoring on the hooks facade tests

--- tests/Integration/Framework/WordPress/Facade/HooksFacadeTest.php
+++ tests/Integration/Framework/WordPress/Facade/HooksFacadeTest.php
@@ -15,33 +15,33 @@ class HooksFacadeTest extends \lucatume\WPBrowser\TestCase\WPTestCase
     {
         $hooksFacade = new HooksFacade();
 
-        $hooksFacade->addFilter('test_filter', '__return_true');
+        $hooksFacade->addFilter('test_filter1', '__return_true');
 
-        $this->assertEquals(10, has_filter('test_filter', '__return_true'));
+        $this->assertEquals(10, has_filter('test_filter1'));
     }
 
     public function testRemoveFilter(): void
     {
         $hooksFacade = new HooksFacade();
 
-        add_filter('test_filter', '__return_true');
+        add_filter('test_filter2', '__return_true');
 
-        $this->assertEquals(10, has_filter('test_filter', '__return_true'));
+        $this->assertEquals(10, has_filter('test_filter2'));
 
-        $hooksFacade->removeFilter('test_filter', '__return_true');
+        $hooksFacade->removeFilter('test_filter2', '__return_true');
 
-        $this->assertEquals(0, has_filter('test_filter', '__return_true'));
+        $this->assertEquals(0, has_filter('test_filter2'));
     }
 
     public function testApplyFilters(): void
     {
         $hooksFacade = new HooksFacade();
 
-        add_filter('test_filter', function ($value) {
-            return $value . 'test';
+        add_filter('test_filter3', function ($value) {
+            return $value . '!';
         });
 
-        $this->assertEquals('testtest', $hooksFacade->applyFilters('test_filter', 'test'));
+        $this->assertEquals('test!', $hooksFacade->applyFilters('test_filter3', 'test'));
     }
 
     public function testAddAction(): void
@@ -49,13 +49,13 @@ class HooksFacadeTest extends \lucatume\WPBrowser\TestCase\WPTestCase
         $hooksFacade = new HooksFacade();
         $output = '';
 
-        add_action('test_action', function () use (&$output) {
-            $output .= 'test_action';
+        add_action('test_action4', function () use (&$output) {
+            $output .= 'test!';
         });
 
-        $hooksFacade->doAction('test_action');
+        $hooksFacade->doAction('test_action4');
 
-        $this->assertEquals('test_action', $output);
+        $this->assertEquals('test!', $output);
     }
 
     public function testRemoveAction(): void
@@ -67,16 +67,16 @@ class HooksFacadeTest extends \lucatume\WPBrowser\TestCase\WPTestCase
             $count++;
         };
 
-        add_action('test_action', $callback);
+        add_action('test_action5', $callback);
 
-        do_action('test_action');
-        do_action('test_action');
+        do_action('test_action5');
+        do_action('test_action5');
 
         $this->assertEquals(2, $count);
 
-        $hooksFacade->removeAction('test_action', $callback);
+        $hooksFacade->removeAction('test_action5', $callback);
 
-        do_action('test_action');
+        do_action('test_action5');
 
         $this->assertEquals(2, $count);
     }
@@ -86,12 +86,12 @@ class HooksFacadeTest extends \lucatume\WPBrowser\TestCase\WPTestCase
         $hooksFacade = new HooksFacade();
         $output = '';
 
-        add_action('test_action', function () use (&$output) {
-            $output .= 'test_action';
+        add_action('test_action6', function () use (&$output) {
+            $output .= 'test_action6';
         });
 
-        $hooksFacade->doAction('test_action');
+        $hooksFacade->doAction('test_action6');
 
-        $this->assertEquals('test_action', $output);
+        $this->assertEquals('test_action6', $output);
     }
 }
