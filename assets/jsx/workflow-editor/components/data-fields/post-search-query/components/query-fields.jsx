6d26349d4d5793ecf657f4e22999e819401d05f8
Anderson Martins <anderson@publishpress.com>
2025-03-04 18:09:29 -0300

    Change the query filters prefix

--- assets/jsx/workflow-editor/components/data-fields/post-search-query/components/query-fields.jsx
+++ assets/jsx/workflow-editor/components/data-fields/post-search-query/components/query-fields.jsx
@@ -3,38 +3,38 @@ import { __ } from '@wordpress/i18n';
 export const queryFields = [
     {
         label: __('Post ID', 'post-expirator'),
-        value: 'post.ID',
+        value: 'p.ID',
     },
     {
         label: __('Post Title', 'post-expirator'),
-        value: 'post.post_title',
+        value: 'p.post_title',
     },
     {
         label: __('Post Content', 'post-expirator'),
-        value: 'post.post_content',
+        value: 'p.post_content',
     },
     {
         label: __('Post Excerpt', 'post-expirator'),
-        value: 'post.post_excerpt',
+        value: 'p.post_excerpt',
     },
     {
         label: __('Post Author', 'post-expirator'),
-        value: 'post.post_author',
+        value: 'p.post_author',
     },
     {
         label: __('Post Date', 'post-expirator'),
-        value: 'post.post_date',
+        value: 'p.post_date',
     },
     {
         label: __('Post Status', 'post-expirator'),
-        value: 'post.post_status',
+        value: 'p.post_status',
     },
     {
         label: __('Post Type', 'post-expirator'),
-        value: 'post.post_type',
+        value: 'p.post_type',
     },
     {
         label: __('Post Parent', 'post-expirator'),
-        value: 'post.post_parent',
+        value: 'p.post_parent',
     },
 ];
