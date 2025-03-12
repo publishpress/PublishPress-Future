8c7ad80ac6c315c3a982b9eb0accedd9ca01255c
Anderson Martins <anderson@publishpress.com>
2025-03-12 09:36:10 -0300

    Error on post edit page when default taxonomy is not found. #1211

--- src/Modules/Expirator/Controllers/BlockEditorController.php
+++ src/Modules/Expirator/Controllers/BlockEditorController.php
@@ -109,7 +109,10 @@ class BlockEditorController implements InitializableInterface
             $taxonomyPluralName = '';
             if (! empty($postTypeDefaultConfig['taxonomy'])) {
                 $taxonomy = get_taxonomy($postTypeDefaultConfig['taxonomy']);
-                $taxonomyPluralName = $taxonomy->label;
+
+                if (is_object($taxonomy)) {
+                    $taxonomyPluralName = $taxonomy->label;
+                }
             }
 
             $taxonomyTerms = [];
