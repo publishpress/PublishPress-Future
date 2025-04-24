bd398e02139a081935b968bb03fc1d2a56eb7c17
Anderson Martins <anderson@publishpress.com>
2025-04-24 12:27:19 -0300

    Refactor makefile and scripts for improved Docker image build and push process. Introduce new build-push-image.sh script, consolidate push commands, and remove obsolete terminal-build-push-image.sh script. Ensure environment variables are loaded correctly in services-build-images.sh.

--- dev-workspace/scripts/terminal-build-push-image.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-TERMINAL_IMAGE_NAME=$1
-
-# This command requires to be logged in on Docker Hub. Check `docker login --help` for more information.
-docker buildx build --platform linux/amd64,linux/arm64 --push -t $TERMINAL_IMAGE_NAME ./docker/terminal
