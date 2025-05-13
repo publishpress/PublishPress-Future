07e08fca2fe5906824873fddf67e3e2a59248272
Anderson Martins <anderson@publishpress.com>
2025-05-13 19:37:13 -0300

    Update Makefile and WP-CLI plugin loading logic
    - Enhanced the Makefile to include separate build and push commands for WP-CLI, improving the build process for WordPress environments.
    - Updated the plugin loading logic in load-mu-plugins.php to reference the correct Spatie Ray plugin path, ensuring proper functionality.

--- dev-workspace/docker/wp/wpcli/load-mu-plugins.php
+++ dev-workspace/docker/wp/wpcli/load-mu-plugins.php
@@ -1,5 +1,5 @@
 <?php
 
-if (file_exists(WPMU_PLUGIN_DIR.'/wordpress-ray/wp-ray.php')) {
-    include_once WPMU_PLUGIN_DIR.'/wordpress-ray/wp-ray.php';
+if (file_exists(WPMU_PLUGIN_DIR.'/spatie-ray/wp-ray.php')) {
+    include_once WPMU_PLUGIN_DIR.'/spatie-ray/wp-ray.php';
 }
